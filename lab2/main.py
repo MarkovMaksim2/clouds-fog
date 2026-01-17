8c8817ac786f8d47511d36d7c950c80cf34a5ca9
Maksim Markov <148797503+MarkovMaksim2@users.noreply.github.com>
2026-01-17 21:47:18 +0300

    feat: add lab2

---
+++ lab2/main.py
@@ -0,0 +1,25 @@
+from flask import Flask
+import redis
+import os
+
+app = Flask(__name__)
+
+redis_host = os.environ.get('REDIS_HOST', 'redis')
+redis_port = int(os.environ.get('REDIS_PORT', 6379))
+redis_client = redis.Redis(host=redis_host, port=redis_port, decode_responses=True)
+
+@app.route('/')
+def hello():
+    count = redis_client.incr('hits')
+    return f'''
+    <html>
+        <body style="text-align: center; font-family: Arial; padding: 50px; background-color: #ff00ff;">
+            <h1>Страничка в интернете</h1>
+            <h2>Количество посещений: {count}</h2>
+            <p>Контейнер ID: {os.environ.get('HOSTNAME', 'Unknown')}</p>
+        </body>
+    </html>
+    '''
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
